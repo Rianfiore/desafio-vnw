7a8dcc61c19631a5d2e3752bfee0e900567acc2f
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-12 17:15:12 -0300

    mocking all component data in one file

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Header from "./header";
+import Footer from "./footer";
+import Form from "./form";
+import Section from "./section";
+import Article from "./article";
+import SearchBar from "./searchBar";
+import Card from "./card";
+import Icon from "./icon";
+
+export { Header, Footer, Form, Section, Article, SearchBar, Card, Icon };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/home";
+import App from "./App";
+
+import "index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
