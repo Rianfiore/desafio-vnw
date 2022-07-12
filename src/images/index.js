61351a9e14ef99aea35c86353a06596afc157de1
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-12 17:19:26 -0300

    importing and exporting all images

---
+++ src/images/index.js
@@ -0,0 +1,34 @@
+import bg from "./bg/bg.png";
+import pcSection from "./pcSection/pc-section.png";
+import fullstackSection from "./fullstackSection/fullstack-section.png";
+import logoHeader from "./logoHeader/logo-header.svg";
+import logoSection from "./logoSection/logo-section.png";
+import searchIcon from "./searchIcon/searchIcon.svg";
+import eventsImage from "./eventsImage/events-image.png";
+import facebook from "./facebook/facebook.png";
+import instagram from "./instagram/insta.png";
+import linkedin from "./linkedin/linkedin.png";
+import medium from "./medium/medium.png";
+import youtube from "./youtube/youtube.png";
+import footerBg from "./footerBg/footerBg.png";
+import sunIcon from "./light/light.svg";
+import moonIcon from "./moon/moon.svg";
+const images = {
+  bg: bg,
+  pcSection: pcSection,
+  fullstackSection: fullstackSection,
+  logoHeader: logoHeader,
+  logoSection: logoSection,
+  searchIcon: searchIcon,
+  eventsImage: eventsImage,
+  facebook: facebook,
+  instagram: instagram,
+  linkedin: linkedin,
+  medium: medium,
+  youtube: youtube,
+  footerBg: footerBg,
+  moonIcon: moonIcon,
+  sunIcon: sunIcon,
+};
+
+export default images;
