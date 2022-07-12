73b09ff9df5edffcaa6c62c9f1eaed5bbb121661
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-12 17:21:23 -0300

    creating dark theme and light theme

---
+++ src/styles/theme.js
@@ -0,0 +1,241 @@
+import image from "images";
+
+export const lightTheme = {
+  images: {
+    main: {
+      bg: {
+        src: image.bg,
+      },
+    },
+  },
+  icon: {
+    size: {
+      small: "10",
+      medium: "12",
+      large: "15",
+    },
+  },
+  colors: {
+    gradient: "#0F2260",
+    nav: {
+      text: "#fff",
+      bg: "#031257",
+      hover: "#ff611E",
+    },
+    main: {
+      title: "#ffac2d",
+      decoration: "#ff611E",
+    },
+    form: {
+      primaryColor: "#fff",
+      secondaryColor: "#00145D",
+      linkColor: "#FED5B2",
+    },
+    section: {
+      title: "#0F2260",
+      subtitle: "#0F2260",
+      paragraph: "#000000",
+      bg: "#fff",
+    },
+    article: {
+      title: "#0F2260",
+      paragraph: "#000000",
+      icon: {
+        primaryColor: "#FDE7A9",
+        secondaryColor: "#FFAC2D",
+      },
+    },
+    card: {
+      title: "#fff",
+      subtitle: "#fff",
+    },
+    squad: {
+      primaryColor: "#0F2260",
+      secondaryColor: "#fff",
+    },
+  },
+  fonts: {
+    family: {
+      primary: "Montserrat",
+    },
+    style: {
+      primary: "normal",
+      italic: "italic",
+    },
+    main: {
+      title: "clamp(20px, 3.5vw, 100px)",
+    },
+    form: {
+      title: "3vw",
+      subtitle: "1.75vw",
+      link: "1.5vw",
+    },
+    section: {
+      title: "2.75vw",
+      subtitle: "1.5vw",
+      paragraph: "1.3vw",
+    },
+    article: {
+      title: "1.5vw",
+      paragraph: "1.1vw",
+    },
+    card: {
+      title: "1.2vw",
+      subtitle: "0.8vw",
+    },
+  },
+  media: {
+    lteMedium: "(max-width: 768px)",
+  },
+  sizes: {
+    xxmicro: "0.01vw",
+    xmicro: "0.05vw",
+    micro: "0.1vw",
+    xxsmall: "0.2vw",
+    xsmall: "0.3vw",
+    small: "0.4vw",
+    medium: "0.75vw",
+    large: "1vw",
+    xlarge: "1.5vw",
+    xxlarge: "2vw",
+    xxxlarge: "4vw",
+    huge: "6vw",
+    xhuge: "12vw",
+    xxhuge: "18vw",
+    xxxhuge: "20vw",
+  },
+  spacings: {
+    xxxsmall: "0.25vw",
+    xxsmall: "0.5vw",
+    xsmall: "0.75vw",
+    small: "1vw",
+    medium: "1.5vw",
+    large: "2vw",
+    xlarge: "3vw",
+    xxlarge: "4vw",
+    huge: "5vw",
+    xhuge: "6vw",
+    xxhuge: "8vw",
+    xxxhuge: "10vw",
+  },
+};
+
+export const darkTheme = {
+  images: {
+    main: {
+      bg: {
+        src: image.bg,
+      },
+    },
+  },
+  icon: {
+    size: {
+      small: "10",
+      medium: "12",
+      large: "15",
+    },
+  },
+  colors: {
+    gradient: "#000",
+    nav: {
+      text: "#000",
+      bg: "#fff",
+      hover: "#000",
+    },
+    main: {
+      title: "#fff",
+      decoration: "#ff611E",
+    },
+    form: {
+      primaryColor: "#303030",
+      secondaryColor: "#fff",
+      linkColor: "#fff",
+    },
+    section: {
+      title: "#ff611E",
+      subtitle: "#fff",
+      paragraph: "#fff",
+      bg: "#031257",
+    },
+    article: {
+      title: "#FFAC2D",
+      paragraph: "#fff",
+      icon: {
+        primaryColor: "#FDE7A9",
+        secondaryColor: "#FFAC2D",
+      },
+    },
+    card: {
+      title: "#fff",
+      subtitle: "#fff",
+    },
+    squad: {
+      primaryColor: "#0F2260",
+      secondaryColor: "#0F2260",
+    },
+  },
+  fonts: {
+    family: {
+      primary: "Montserrat",
+    },
+    style: {
+      primary: "normal",
+      italic: "italic",
+    },
+    main: {
+      title: "clamp(20px, 3.5vw, 100px)",
+    },
+    form: {
+      title: "3vw",
+      subtitle: "1.75vw",
+      link: "1.5vw",
+    },
+    section: {
+      title: "2.75vw",
+      subtitle: "1.5vw",
+      paragraph: "1.3vw",
+    },
+    article: {
+      title: "1.5vw",
+      paragraph: "1.1vw",
+    },
+    card: {
+      title: "1.2vw",
+      subtitle: "0.8vw",
+    },
+  },
+  media: {
+    lteMedium: "(max-width: 768px)",
+  },
+  sizes: {
+    xxmicro: "0.01vw",
+    xmicro: "0.05vw",
+    micro: "0.1vw",
+    xxsmall: "0.2vw",
+    xsmall: "0.3vw",
+    small: "0.4vw",
+    medium: "0.75vw",
+    large: "1vw",
+    xlarge: "1.5vw",
+    xxlarge: "2vw",
+    xxxlarge: "4vw",
+    huge: "6vw",
+    xhuge: "12vw",
+    xxhuge: "18vw",
+    xxxhuge: "20vw",
+  },
+  spacings: {
+    xxxsmall: "0.25vw",
+    xxsmall: "0.5vw",
+    xsmall: "0.75vw",
+    small: "1vw",
+    medium: "1.5vw",
+    large: "2vw",
+    xlarge: "3vw",
+    xxlarge: "4vw",
+    huge: "5vw",
+    xhuge: "6vw",
+    xxhuge: "8vw",
+    xxxhuge: "10vw",
+  },
+};
