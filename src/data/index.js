382e1638669d9dcb4b6005dde0cb0a158b71adf1
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-12 17:18:14 -0300

    creating all cards data

---
+++ src/data/index.js
@@ -0,0 +1,79 @@
+import images from "./imgSquad";
+
+export const squad = [
+  {
+    name: "Dimitri Duque",
+    role: "voluntários",
+    img: images[0],
+  },
+  {
+    name: "Hugo Sabino",
+    role: "voluntários",
+    img: images[1],
+  },
+  {
+    name: "Karla De Melo",
+    role: "voluntários",
+    img: images[2],
+  },
+  {
+    name: "Paula Misan",
+    role: "voluntários",
+    img: images[3],
+  },
+  {
+    name: "Thais Brait",
+    role: "voluntários",
+    img: images[4],
+  },
+  {
+    name: "Aline Fróes",
+    role: "CEO",
+    img: images[5],
+  },
+  {
+    name: "Aline Nogueira",
+    role: "Agente Socioemocional",
+    img: images[6],
+  },
+  {
+    name: "Andrea Marinho",
+    role: "Psicóloga",
+    img: images[7],
+  },
+  {
+    name: "Cris Dos Prazeres",
+    role: "Coordenadora Executiva",
+    img: images[8],
+  },
+  {
+    name: "Karynne Moreira",
+    role: "Facilitadora Técnica",
+    img: images[9],
+  },
+  {
+    name: "Luzia Merlim",
+    role: "Agente Socioemocional",
+    img: images[10],
+  },
+  {
+    name: "Mariana Correia",
+    role: "Coordenadora Administrativa",
+    img: images[11],
+  },
+  {
+    name: "Marina Tiago",
+    role: "Agente de Estratégia",
+    img: images[12],
+  },
+  {
+    name: "Marlon Yuri",
+    role: "Instrutor Técnico",
+    img: images[13],
+  },
+  {
+    name: "Priscila Tufani",
+    role: "Agente de Estratégia",
+    img: images[14],
+  },
+];
